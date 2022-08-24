a0d522b1415c75ee845f0f89c53b2203ca8b9da9
TMWAG <semyon.valyaev@gamil.com>
2022-08-24 23:20:47 +0300

    changed tab width for prettier from 4 to 2, reformatted

--- server/index.js
+++ server/index.js
@@ -7,11 +7,11 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 const start = async () => {
-    try {
-        await sequelize.authenticate();
-        await sequelize.sync();
-        app.listen(PORT, () => console.log(`server started on ${PORT} port`));
-    } catch (e) {}
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => console.log(`server started on ${PORT} port`));
+  } catch (e) {}
 };
 
 start();
