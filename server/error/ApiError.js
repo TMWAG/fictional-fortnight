76e0baf74e5f9163a0f05b846dea8ed24bde070b
TMWAG <semyon.valyaev@gmail.com>
2022-08-26 18:42:27 +0300

    added error handler

---
+++ server/error/ApiError.js
@@ -0,0 +1,19 @@
+class ApiError extends Error {
+  constructor(status, message) {
+    super();
+    this.status = status;
+    this.message = message;
+  }
+
+  static badRequest(msg) {
+    return new ApiError(404, msg);
+  }
+
+  static forbidden(msg) {
+    return new ApiError(403, msg);
+  }
+
+  static internal(msg) {
+    return new ApiError(500, msg);
+  }
+}
