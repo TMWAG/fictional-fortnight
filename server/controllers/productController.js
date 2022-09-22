72799a2c1ebee16c1bc6b27d894894730ec079fd
TMWAG <semyon.valyaev@gmail.com>
2022-09-22 23:42:50 +0300

    finished work on products router and controller

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -14,51 +14,155 @@ class ProductController {
         categoryId,
       });
       return res.json(product);
-    } catch (e) {
-      next(ApiError.badRequest(e.message));
+    } catch (error) {
+      next(ApiError.badRequest(error.message));
     }
   }
 
-  async getAll(req, res) {
-    let { vendorId, categoryId, limit, page } = req.query;
-    page = page || 1;
-    limit = limit || 12;
-    let offset = page * limit - limit;
-    let products;
-    if (!vendorId && !categoryId) {
-      products = await Product.findAndCountAll({ limit, offset });
-    }
-    if (vendorId && !categoryId) {
-      products = await Product.findAndCountAll({
-        where: { vendorId },
-        limit,
-        offset,
-      });
-    }
-    if (!vendorId && categoryId) {
-      products = await Product.findAndCountAll({
-        where: { categoryId },
-        limit,
-        offset,
-      });
-    }
-    if (vendorId && categoryId) {
-      products = await Product.findAndCountAll({
-        where: { vendorId, categoryId },
-        limit,
-        offset,
-      });
-    }
-    return res.json(products);
-  }
-
-  async getById(req, res) {
+  async getById(req, res, next) {
     try {
       const { id } = req.params;
       const product = await Product.findOne({ where: { id } });
       return res.json(product);
     } catch (error) {
-      console.log(error);
+      next(ApiError.badRequest(error.message));
+    }
+  }
+
+  async getAll(req, res, next) {
+    try {
+      let { vendorId, categoryId, limit, page } = req.query;
+      page = page || 1;
+      limit = limit || 12;
+      let offset = page * limit - limit;
+      let products;
+      if (!vendorId && !categoryId) {
+        products = await Product.findAndCountAll({ limit, offset });
+      }
+      if (vendorId && !categoryId) {
+        products = await Product.findAndCountAll({
+          where: { vendorId },
+          limit,
+          offset,
+        });
+      }
+      if (!vendorId && categoryId) {
+        products = await Product.findAndCountAll({
+          where: { categoryId },
+          limit,
+          offset,
+        });
+      }
+      if (vendorId && categoryId) {
+        products = await Product.findAndCountAll({
+          where: { vendorId, categoryId },
+          limit,
+          offset,
+        });
+      }
+      return res.json(products);
+    } catch (error) {
+      next(ApiError.badRequest(error.message));
+    }
+  }
+
+  async getAllByVendorId(req, res, next) {
+    try {
+      const { vendorId } = req.params;
+      let { limit, offset } = req.query;
+      const products = await Product.findAndCountAll({
+        where: { vendorId },
+        limit,
+        offset,
+      });
+      return res.json(products);
+    } catch (error) {
+      next(ApiError.badRequest(error.message));
+    }
+  }
+
+  async getAllByCategoryId(req, res, next) {
+    try {
+      const { categoryId } = req.params;
+      let { limit, offset } = req.query;
+      const products = await Product.findAndCountAll({
+        where: { categoryId },
+        limit,
+        offset,
+      });
+      return res.json(products);
+    } catch (error) {
+      next(ApiError.badRequest(error.message));
+    }
+  }
+
+  async updateNameByProductId(req, res, next) {
+    try {
+      const { id, name } = req.body;
+      const product = await Product.update({ name }, { where: { id } });
+      return res.json(product);
+    } catch (error) {
+      next(ApiError.badRequest(error.message));
+    }
+  }
+
+  async updatePriceByProductId(req, res, next) {
+    try {
+      const { id, price } = req.body;
+      const product = await Product.update({ price }, { where: { id } });
+      return res.json(product);
+    } catch (error) {
+      next(ApiError.badRequest(error.message));
+    }
+  }
+
+  async updateAmountByProductId(req, res, next) {
+    try {
+      const { id, amount } = req.body;
+      const product = await Product.update({ amount }, { where: { id } });
+      return res.json(product);
+    } catch (error) {
+      next(ApiError.badRequest(error.message));
+    }
+  }
+
+  async updateDiscountByProductId(req, res, next) {
+    try {
+      const { id, discount } = req.body;
+      const product = await Product.update({ discount }, { where: { id } });
+      return res.json(product);
+    } catch (error) {
+      next(ApiError.badRequest(error.message));
+    }
+  }
+
+  async updateVendorByProductId(req, res, next) {
+    try {
+      const { id, vendorId } = req.body;
+      const product = await Product.update({ vendorId }, { where: { id } });
+      return res.json(product);
+    } catch (error) {
+      next(ApiError.badRequest(error.message));
+    }
+  }
+
+  async updateCategoryByProductId(req, res, next) {
+    try {
+      const { id, categoryId } = req.body;
+      const product = await Product.update({ categoryId }, { where: { id } });
+      return res.json(product);
+    } catch (error) {
+      next(ApiError.badRequest(error.message));
+    }
+  }
+
+  async deleteByProductId(req, res, next) {
+    try {
+      const { id } = req.body;
+      const product = await Product.destroy({ where: { id } });
+      return res.json(product);
+    } catch (error) {
+      next(ApiError.badRequest(error.message));
     }
   }
 }
--- server/routes/productRouter.js
+++ server/routes/productRouter.js
@@ -1,9 +1,50 @@
 const Router = require('express');
 const router = new Router();
 const productController = require('../controllers/productController');
+const checkRoleMiddleware = require('../middleware/checkRoleMiddleware');
 
-router.post('/', productController.create);
-router.get('/', productController.getAll);
-router.get('/:id', productController.getById);
+router.post('/create', checkRoleMiddleware(1), productController.create);
+
+router.get('/item/:id', productController.getById);
+router.get('/all', productController.getAll);
+router.get('/vendor/:vendorId', productController.getAllByVendorId);
+router.get('/category/:categoryId', productController.getAllByCategoryId);
+
+router.put(
+  '/updateName',
+  checkRoleMiddleware(1),
+  productController.updateNameByProductId
+);
+router.put(
+  '/updatePrice',
+  checkRoleMiddleware(1),
+  productController.updatePriceByProductId
+);
+router.put(
+  '/updateAmount',
+  checkRoleMiddleware(1),
+  productController.updateAmountByProductId
+);
+router.put(
+  '/updateDiscount',
+  checkRoleMiddleware(1),
+  productController.updateDiscountByProductId
+);
+router.put(
+  '/updateVendor',
+  checkRoleMiddleware(1),
+  productController.updateVendorByProductId
+);
+router.put(
+  '/updateCategory',
+  checkRoleMiddleware(1),
+  productController.updateCategoryByProductId
+);
+
+router.delete(
+  '/delete',
+  checkRoleMiddleware(1),
+  productController.deleteByProductId
+);
 
 module.exports = router;
