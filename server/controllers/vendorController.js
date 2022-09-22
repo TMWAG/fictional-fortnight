8235d72ae1531b649e445744ce9f8b3cf46f4abf
TMWAG <semyon.valyaev@gmail.com>
2022-09-23 00:14:19 +0300

    finished work on vendor controller and router

--- server/controllers/vendorController.js
+++ server/controllers/vendorController.js
@@ -2,15 +2,43 @@ const { Vendor } = require('../models/models');
 const ApiError = require('../error/ApiError');
 
 class VendorController {
-  async create(req, res) {
-    const { vendorName } = req.body;
-    const vendor = await Vendor.create({ vendorName });
-    return res.json(vendor);
+  async create(req, res, next) {
+    try {
+      const { vendorName } = req.body;
+      const vendor = await Vendor.create({ vendorName });
+      return res.json(vendor);
+    } catch (error) {
+      next(ApiError.badRequest(error.message));
+    }
   }
 
-  async getAll(req, res) {
-    const vendors = await Vendor.findAll();
-    return res.json(vendors);
+  async getAll(req, res, next) {
+    try {
+      const vendors = await Vendor.findAll();
+      return res.json(vendors);
+    } catch (error) {
+      next(ApiError.badRequest(error.message))
+    }
+  }
+
+  async updateVendorNameById(req, res, next){
+    try {
+      const {vendorName, id} = req.body;
+      const vendor = await Vendor.update({vendorName}, {where:{id}});
+      return res.json(vendor);
+    } catch (error) {
+      next(ApiError.badRequest(error));
+    }
+  }
+
+  async deleteVendorById(req, res, next){
+    try {
+      const {id} = req.body;
+      const vendor = Vendor.destroy({where:{id}});
+      return res.json(vendor);
+    } catch (error) {
+      next(ApiError.badRequest(error.message));
+    }
   }
 }
 
--- server/routes/vendorRouter.js
+++ server/routes/vendorRouter.js
@@ -1,8 +1,14 @@
 const Router = require('express');
 const router = new Router();
 const vendorController = require('../controllers/vendorController');
+const checkRoleMiddleware = require('../middleware/checkRoleMiddleware');
 
-router.post('/', vendorController.create);
-router.get('/', vendorController.getAll);
+router.post('/create', checkRoleMiddleware(1), vendorController.create);
+
+router.get('/all', vendorController.getAll);
+
+router.put('/updateName', checkRoleMiddleware(1), vendorController.updateVendorNameById);
+
+router.delete('/delete', checkRoleMiddleware(1), vendorController.deleteVendorById);
 
 module.exports = router;
